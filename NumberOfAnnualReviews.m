d03ef4509c2288cf2511be574b06cc944eba7125
kovanostra <kovanostra@users.noreply.github.com>
2016-08-22 17:18:58 +0200

    Update NumberOfAnnualReviews.m

--- NumberOfAnnualReviews.m
+++ NumberOfAnnualReviews.m
@@ -2,11 +2,11 @@ function [ reviews ] = NumberOfAnnualReviews( yearlySubmissions, secondRoundProb
 
 reviewsAndRevisions = round(yearlySubmissions.*resubmissionProbability);
 secondRoundPapers = secondRoundProbability.*reviewsAndRevisions(1);
-reviews = inHouseAcceptanceRate.*2.3.*(round(reviewsAndRevisions(1)) + secondRoundPapers);
+reviews = inHouseAcceptanceRate.*2.5.*(round(reviewsAndRevisions(1)) + secondRoundPapers);
 
 for iRevisions = 2:length(reviewsAndRevisions)
     secondRoundPapers = secondRoundProbability.*reviewsAndRevisions(iRevisions);
-    reviews = reviews + inHouseAcceptanceRate.*2.3.*(iRevisions*reviewsAndRevisions(iRevisions) + secondRoundPapers);
+    reviews = reviews + inHouseAcceptanceRate.*2.5.*(iRevisions*reviewsAndRevisions(iRevisions) + secondRoundPapers);
 end
 
 end
